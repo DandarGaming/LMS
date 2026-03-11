225f93d063d8cde5a4927c6358dbf69953097d8d
Dandar <103852756+DandarGaming@users.noreply.github.com>
2026-03-11 10:39:50 +0000

    Create Loan class with member and book attributes
    Initial implementation of Loan class with member and book attributes.

---
+++ Loan.java
@@ -0,0 +1,34 @@
+public class Loan {
+
+    private Member member;
+    private Book book;
+
+    public Loan(Member member, Book book) {
+
+    }
+
+    public Member getMember() {
+
+    }
+
+    public void setMember(Member member) {
+
+    }
+
+    public Book getBook() {
+
+    }
+
+    public void setBook(Book book) {
+
+    }
+
+    public String getLoanDetails() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
