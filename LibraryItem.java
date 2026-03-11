9ce34db0d8fe82155aa19f4057a6bf37779f8b07
DanD2007-LOETB <2025ddunne@loetbstudent.ie>
2026-03-11 10:54:18 +0000

    Update LibraryItem.java
    added commented guide on what needs to be coded in, aswell as adding scanner class and some sout to start from

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,35 +1,38 @@
 public class LibraryItem {
 
+import java.util.Scanner;
+    
     private int itemId;
     private String title;
     private boolean available;
 
     public LibraryItem(int itemId, String title, boolean available) {
-
+        // sout inputted id, inputted string, inputed availability
     }
 
     public int getItemId() {
-
+        System.out.prntln("Enter Item ID: ")
     }
 
     public void setItemId(int itemId) {
-
+        // set itemId to the user input
     }
 
     public String getTitle() {
-
+        System.out.println("Enter The Title: ")
     }
 
     public void setTitle(String title) {
-
+        // set title to user input
     }
 
     public boolean isAvailable() {
-
+        System.out.println("Is the book currently available? (Y/N): ")
     }
 
     public void setAvailable(boolean available) {
-
+        // if input = Y set book to available
+        // if input = N set book to unavailable
     }
 
     @Override
