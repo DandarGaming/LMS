1fb9e2de172bcb8df4a804f8e0d9e3af6294ef2f
Dandar <103852756+DandarGaming@users.noreply.github.com>
2026-03-11 10:38:35 +0000

    Add Book class extending LibraryItem

---
+++ Book.java
@@ -0,0 +1,30 @@
+public class Book extends LibraryItem {
+
+    private String author;
+    private String isbn;
+
+    public Book(int itemId, String title, boolean available, String author, String isbn) {
+        super(itemId, title, available);
+    }
+
+    public String getAuthor() {
+
+    }
+
+    public void setAuthor(String author) {
+
+    }
+
+    public String getIsbn() {
+
+    }
+
+    public void setIsbn(String isbn) {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
