20ba50bb385b502d49ce3166daf43deb1540546b
Dandar <103852756+DandarGaming@users.noreply.github.com>
2026-03-11 10:39:00 +0000

    Create Member class with member attributes
    Define Member class with attributes and method stubs.

---
+++ Member.java
@@ -0,0 +1,52 @@
+public class Member {
+
+    private String name;
+    private int memberId;
+    private String email;
+    private int borrowedBooks;
+
+    public Member(String name, int memberId, String email, int borrowedBooks) {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public int getMemberId() {
+
+    }
+
+    public void setMemberId(int memberId) {
+
+    }
+
+    public String getEmail() {
+
+    }
+
+    public void setEmail(String email) {
+
+    }
+
+    public int getBorrowedBooks() {
+
+    }
+
+    public void setBorrowedBooks(int borrowedBooks) {
+
+    }
+
+    public int calculateBorrowLimit() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
