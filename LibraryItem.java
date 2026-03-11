091a07a63c61fbe3a7944c59a2e35c4b5c5c14ab
Dandar <103852756+DandarGaming@users.noreply.github.com>
2026-03-11 10:37:50 +0000

    Add LibraryItem class with basic properties and methods

---
+++ LibraryItem.java
@@ -0,0 +1,39 @@
+public class LibraryItem {
+
+    private int itemId;
+    private String title;
+    private boolean available;
+
+    public LibraryItem(int itemId, String title, boolean available) {
+
+    }
+
+    public int getItemId() {
+
+    }
+
+    public void setItemId(int itemId) {
+
+    }
+
+    public String getTitle() {
+
+    }
+
+    public void setTitle(String title) {
+
+    }
+
+    public boolean isAvailable() {
+
+    }
+
+    public void setAvailable(boolean available) {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
