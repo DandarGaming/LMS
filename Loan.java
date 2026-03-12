5f1bf3bfc1bc4ccf1a73a1b56df444b1588c7626
Jon40K <2025jomurphy@loetbstudent.ie>
2026-03-12 22:34:53 +0000

    Refactor Loan class for member and book management
    Refactor Loan class methods to handle member and book input directly from the user. Remove unused methods and comments.

--- Loan.java
+++ Loan.java
@@ -1,34 +1,65 @@
+import java.util.Scanner;
+
 public class Loan {
 
     private Member member;
     private Book book;
+    
+    static Scanner scanner = new Scanner(System.in);
+    static int userIntInput;
+    static boolean exitProgram = false;
+    static int totalBooks = 0;
+    static String bookTitle;
+    static String bookAuthor;
+    static String bookIsbn;
+    static String memberName;
+    static int memberId;
+    static String memberEmail;
+
 
     public Loan(Member member, Book book) {
 
     }
 
-    public Member getMember() {
+    // public Member getMember() {
 
-    }
+    // }
 
-    public void setMember(Member member) {
-
+    public void setMember() {
+        System.out.println("Enter the name of the new member: ");
+        memberName = scanner.nextLine();
+        System.out.println("Enter the ID for this new member: ");
+        memberId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the new members email: ");
+        memberEmail = scanner.nextLine();
+        Member newmember = new Member(memberName, memberId, memberEmail, 0); 
     }
 
     public Book getBook() {
-
+        String title = scanner.nextLine();
+        book = Book.GetBook(title);
+        return book;
     }
 
-    public void setBook(Book book) {
-
+    public void setBook() {
+        System.out.println("Enter the title of the book: ");
+        bookTitle = scanner.nextLine();
+        System.out.println("Enter the name of the author: ");
+        bookAuthor = scanner.nextLine();
+        System.out.println("Enter the books Isbn: ");
+        bookIsbn = scanner.nextLine();
+        Book.AddBook(totalBooks + 1, bookTitle, true, bookAuthor, bookIsbn);
     }
 
     public String getLoanDetails() {
-
+        System.out.println("Enter the name of the person who you wish to check the loan details of");
+        memberName = scanner.nextLine();
+        return Member.getBorrowedBooks();
     }
 
-    @Override
-    public String toString() {
-
-    }
+    // @Override
+    // public String toString() {
+    // Does this file need a toString??? It stores no constant information on anything
+    // }
 }
