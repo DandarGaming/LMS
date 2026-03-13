c1e69f1b0f0e5c5d3bac6a08c8e12e51a5bf7cb8
Jon40K <2025jomurphy@loetbstudent.ie>
2026-03-13 19:52:19 +0000

    Refactor Loan class to improve member management

--- Loan.java
+++ Loan.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Loan {
 
@@ -16,43 +17,73 @@ public class Loan {
     static int memberId;
     static String memberEmail;
 
+    private static final HashMap<String, Member> members = new HashMap<>();
+
 
     public Loan(Member member, Book book) {
-
+        this.member = member;
+        this.book = book;
     }
 
     public Member getMember() {
-        
+        return this.member;
     }
 
     public void setMember() {
-        System.out.println("Enter the name of the new member");
+        System.out.println("Enter the number of the member option you wish to add"
+            + "\n1. Staff Member"
+            + "\n2. Student Member"
+        );
+        userIntInput = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the name of the new member: ");
         memberName = scanner.nextLine();
-        System.out.println("");
-
+        System.out.println("Enter the ID for this new member: ");
+        memberId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the new members email: ");
+        memberEmail = scanner.nextLine();
+        switch(userIntInput) {
+            case 1:
+                //Staff Member
+                StaffMember newStaffMember = new StaffMember(memberName, memberId, memberEmail, memberId);
+                members.put(memberName, newStaffMember);
+                break;
+            case 2:
+                //Student Member
+                StudentMember newStudentMember = new StudentMember(memberName, memberId, memberEmail, memberId);
+                members.put(memberName, newStudentMember);
+                break;   
+        }
+        System.out.println("You have successfully added the member " + members.get(memberName));
     }
 
-    public static String getBook() {
-        System.out.println("Enter the title of the book you wish to view: ");
-        String title = scanner.nextLine();
-        String bookInfo = Book.GetBook(title);
-        return bookInfo;
-        
+    public static String getBook(String title) {
+        if (Book.GetAllBooks() == null) {
+            return "Sorry but no books are in the system";
+        }
+        else {
+            String bookInfo = Book.GetBook(title);
+            return bookInfo;
+        }
     }
 
-    static public void setBook() {
+    public void setBook() {
         System.out.println("Enter the title of the book: ");
         bookTitle = scanner.nextLine();
         System.out.println("Enter the name of the author: ");
         bookAuthor = scanner.nextLine();
         System.out.println("Enter the books Isbn: ");
         bookIsbn = scanner.nextLine();
-        Book.AddBook(totalBooks + 1, bookTitle, true, bookAuthor, bookIsbn);
+        totalBooks++;
+        Book.AddBook(totalBooks, bookTitle, true, bookAuthor, bookIsbn);
     }
 
-    public String getLoanDetails() {
-        return Member.getBorrowedBooks();
-    }
+    // public String getLoanDetails() {
+    //     System.out.println("Enter the name of the person who you wish to check the loan details of");
+    //     memberName = scanner.nextLine();
+    //     return Member.getBorrowedBooks();
+    // }
 
     // @Override
     // public String toString() {
