5bb7b2efd454297ffa2f08802bd99e14786804e0
Jon40K <2025jomurphy@loetbstudent.ie>
2026-03-20 12:15:51 +0000

    Enhance member management with setMember method
    Refactored member management methods and added functionality to set member details.

--- Member.java
+++ Member.java
@@ -9,8 +9,16 @@ public class Member {
     static int memberId;
     static String email;
     static int borrowedBooks;
+
+    static int userIntInput;
+    static int totalBooks = 0;
+    static String memberName;
+    static int memberId;
+    static String memberEmail;
     
     static Scanner scanner = new Scanner(System.in);
+
+    private static final HashMap<String, Member> members = new HashMap<>();
 
     public Member(String name, int memberId, String email, int borrowedBooks) {
         Member.name = name;
@@ -20,20 +28,45 @@ public class Member {
     }
     
     public static void getMember() {
-        System.out.println("Enter the Members Name: \n");
-        name = scanner.nextLine();
-        System.out.println("Enter the Members ID: \n");
-        memberId = scanner.nextInt();
-        System.out.println("Enter the Members Email: \n");
-        email = scanner.nextLine();
+        return this.member;
     }
             
     public String getName() {
         return Member.name;
     }
 
-    public void setName(String name) {
-        Member.name = name;
+    public static void setMember() {
+        //Outputs instructions on how to create a member and gets a user input results
+        System.out.println("""
+                           Enter the number of the member option you wish to add
+                           1. Staff Member
+                           2. Student Member""");
+        userIntInput = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the name of the -new member: ");
+        memberName = scanner.nextLine();
+        System.out.println("Enter the ID for this new member: ");
+        memberId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the new members email: ");
+        memberEmail = scanner.nextLine();
+        //checks the users input to call the right class's constructor
+        switch(userIntInput) {
+            case 1 -> {
+                //Calls the StaffMember constructor
+                StaffMember newStaffMember = new StaffMember(memberName, memberId, memberEmail, 0);
+                //Adds the new StaffMember into the member hashmap under the name the user inputed
+                members.put(memberName, newStaffMember);
+            }
+            case 2 -> {
+                //Calls the StudentMember constructor
+                StudentMember newStudentMember = new StudentMember(memberName, memberId, memberEmail, 0);
+                //Adds the new StudentMember into the member hashmap under the name the user inputed
+                members.put(memberName, newStudentMember);
+            }   
+        }
+        //Outputs a message to alert the user it was successful
+        System.out.println("You have successfully added the member " + members.get(memberName));
     }
 
     public int getMemberId() {
