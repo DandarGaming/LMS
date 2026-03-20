cef97d98123e98c30be70acef0cc36c817d17f26
Jon40K <2025jomurphy@loetbstudent.ie>
2026-03-20 12:24:42 +0000

    Refactor Member class by removing unused code
    Removed unused static member variables and the setMember method that handled member creation and input.

--- Member.java
+++ Member.java
@@ -1,25 +1,11 @@
 package com.example;
 
-import static com.example.Loan.memberEmail;
-import static com.example.Loan.memberId;
-import static com.example.Loan.memberName;
-import static com.example.Loan.scanner;
-import static com.example.Loan.userIntInput;
-import java.util.HashMap;
-
 public class Member {
 
     private String name;
     private int memberId;
     private String email;
     private int borrowedBooks;
-    
-    static String newMemberName;
-    static int newMemberId;
-    static String newMemberEmail;
-    
-     //Initalize a hashamp for Member objects
-    private static final HashMap<String, Member> members = new HashMap<>();
 
     public Member(String name, int memberId, String email, int borrowedBooks) {
         this.name = name;
@@ -30,40 +16,6 @@ public class Member {
 
     public String getName() {
         return this.name;
-    }
-    
-     public static void setMember() {
-        //Outputs instructions on how to create a member and gets a user input results
-        System.out.println("""
-                           Enter the number of the member option you wish to add
-                           1. Staff Member
-                           2. Student Member""");
-        userIntInput = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the name of the -new member: ");
-        newMemberName = scanner.nextLine();
-        System.out.println("Enter the ID for this new member: ");
-        newMemberId = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter the new members email: ");
-        newMemberEmail = scanner.nextLine();
-        //checks the users input to call the right class's constructor
-        switch(userIntInput) {
-            case 1 -> {
-                //Calls the StaffMember constructor
-                StaffMember newStaffMember = new StaffMember(newMemberName, newMemberId, newMemberEmail, 0);
-                //Adds the new StaffMember into the member hashmap under the name the user inputed
-                members.put(memberName, newStaffMember);
-            }
-            case 2 -> {
-                //Calls the StudentMember constructor
-                StudentMember newStudentMember = new StudentMember(newMemberName, newMemberId, newMemberEmail, 0);
-                //Adds the new StudentMember into the member hashmap under the name the user inputed
-                members.put(memberName, newStudentMember);
-            }   
-        }
-        //Outputs a message to alert the user it was successful
-        System.out.println("You have successfully added the member " + members.get(memberName));
     }
 
     public void setName(String name) {
