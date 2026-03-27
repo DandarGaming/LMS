07430337dfd783e99de3a159d98aef22c6eebc5a
Dandar <103852756+DandarGaming@users.noreply.github.com>
2026-03-27 09:35:57 +0000

    Update MemberMenu.java

--- GUI/MemberMenu.java
+++ GUI/MemberMenu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.rectangle;  /*Rename to appropriate package */
+package com.mycompany.lms;
 
 /**
  *
